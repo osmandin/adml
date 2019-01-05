52e779fe0cfa219fc9f3e993cf8ae188454e3926
Osman Din <osmandin@gmail.com>
2019-01-04 21:06:41 -0500

    Make it possible to run without connection.props

--- src/main/java/edu/mit/adml/properties/PropertiesConfigurationUtil.java
+++ src/main/java/edu/mit/adml/properties/PropertiesConfigurationUtil.java
@@ -1,6 +1,8 @@
 package edu.mit.adml.properties;
 
+import edu.mit.adml.DatabaseInitializer;
 import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.DatabaseConfiguration;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,21 +19,33 @@ public class PropertiesConfigurationUtil {
 
 
     static {
+        boolean debug = DatabaseInitializer.isDEBUG();
+
         try {
             login = new PropertiesConfiguration("connection.props");
+            //TODO merge connection.props with application-prod.properties
         } catch (Exception e) {
             logger.error("Error setting property file:", e);
-            logger.error("Exiting due to configuration error.");
-            System.exit(1);
+
+            if (!debug) { // if connection.props is not present
+                logger.error("Exiting due to configuration error.");
+                System.exit(1);
+            }
         }
     }
 
     public static Credentials getCredentials() {
         final Credentials credentials = new Credentials();
-        credentials.setPassword(login.getProperty("login_password").toString());
-        credentials.setUrl(login.getProperty("login_url").toString());
-        credentials.setUsername_app(login.getProperty("app_username").toString());
-        credentials.setPassword_app(login.getProperty("app_password").toString());
+
+        if (login != null) {
+            credentials.setPassword(login.getProperty("login_password").toString());
+            credentials.setUrl(login.getProperty("login_url").toString());
+            credentials.setUsername_app(login.getProperty("app_username").toString());
+            credentials.setPassword_app(login.getProperty("app_password").toString());
+        } else {
+            logger.info("No information provided for ArchivesSpace. Provide connection.props.");
+        }
+
         return credentials;
     }
 }
