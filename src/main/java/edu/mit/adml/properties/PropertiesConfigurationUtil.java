e6c3bdde72dd2fa05cd54d21cdfa12ed0843f9d1
Osman Din <osmandin@gmail.com>
2019-01-04 21:13:17 -0500

    Provide dummy connection.properties

--- src/main/java/edu/mit/adml/properties/PropertiesConfigurationUtil.java
+++ src/main/java/edu/mit/adml/properties/PropertiesConfigurationUtil.java
@@ -2,7 +2,6 @@ package edu.mit.adml.properties;
 
 import edu.mit.adml.DatabaseInitializer;
 import org.apache.commons.configuration.Configuration;
-import org.apache.commons.configuration.DatabaseConfiguration;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,12 +21,12 @@ public class PropertiesConfigurationUtil {
         boolean debug = DatabaseInitializer.isDEBUG();
 
         try {
-            login = new PropertiesConfiguration("connection.props");
-            //TODO merge connection.props with application-prod.properties
+            login = new PropertiesConfiguration("connection.properties");
+            //TODO merge connection.properties with application-prod.properties
         } catch (Exception e) {
             logger.error("Error setting property file:", e);
 
-            if (!debug) { // if connection.props is not present
+            if (!debug) { // if connection.properties is not present
                 logger.error("Exiting due to configuration error.");
                 System.exit(1);
             }
@@ -43,7 +42,7 @@ public class PropertiesConfigurationUtil {
             credentials.setUsername_app(login.getProperty("app_username").toString());
             credentials.setPassword_app(login.getProperty("app_password").toString());
         } else {
-            logger.info("No information provided for ArchivesSpace. Provide connection.props.");
+            logger.info("No information provided for ArchivesSpace. Provide connection.properties.");
         }
 
         return credentials;
